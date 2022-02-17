ff9dc730572f07c261b677626a0ca050bfccefb5
Администратор <anton.iv90@mail.ru>
2022-02-17 21:12:14 +0300

    17.02.2022

--- Tasks/c0_fib.py
+++ Tasks/c0_fib.py
@@ -16,5 +16,13 @@ def fib_iterative(n: int) -> int:
     :param n: number of item
     :return: Fibonacci number
     """
-    print(n)
-    return 0
+    a = 0
+    b = 1
+    if n == 1:
+        return  a
+    if n == 2:
+        return b
+    for _ in range(2, n):
+        a, b = b, a + b
+
+def fib_generator()
