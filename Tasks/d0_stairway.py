a9aae49b95f1837c7ea62c4c4f3c701470d96732
Администратор <anton.iv90@mail.ru>
2022-02-25 18:55:03 +0300

    d0

--- Tasks/d0_stairway.py
+++ Tasks/d0_stairway.py
@@ -9,4 +9,20 @@ def stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
     :return: minimal cost of getting to the top
     """
     print(stairway)
-    return 0
+
+    return direct_stairway_path(stairway)
+
+
+def direct_stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
+    """прямой метод расчета"""
+    #stairway # цены на ступени
+    count_stairs = len(stairway)
+    total_cost = [float("inf")]*count_stairs # стоимость перемещения по ступеням
+
+    total_cost[0] =stairway[0] # начальные условия для первой ступени
+    total_cost[1] = min(stairway[1], stairway[0]+stairway[1]) #начальные условия для второй ступени
+
+    # i - цены ступени + min (стоимость i-1, стоимость i-2)
+    for i in range(2, count_stairs): #len(stairway)
+        total_cost[i] = stairway[i] + min(total_cost[i-1],total_cost[i-2])
+    return  total_cost[-1] #total_cost[count_stairs]
