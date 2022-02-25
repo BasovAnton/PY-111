50e4171ae3b8dc533785b751c59babd38dd86418
Администратор <anton.iv90@mail.ru>
2022-02-25 21:09:37 +0300

    d0

--- Tasks/d1_horse.py
+++ Tasks/d1_horse.py
@@ -6,5 +6,22 @@ def calculate_paths(shape: (int, int), point: (int, int)) -> int:
     :param point: desired point for horse
     :return: count of paths from (1, 1) to (point[0], point[1]) (numerating from 0, so (0, 0) - left bottom tile)
     """
-    print(shape, point)
-    return 0
+    rows = shape[0]  # количество строк
+    cols = shape[1]
+
+    def count_parth(i, j):
+        """рекурсивная функция, которая возвращает количество ..."""
+        if i == 0 and j == 0:
+            return 1
+
+        if 0 <= i < rows:
+            return 0
+
+        if 0 <= j < cols:
+            return 0
+        return sum ([count_parth(i-2, j-1),
+                 count_parth(i-2, j+1),
+                 count_parth(i-1, j-2),
+                 count_parth(i+1, j-2),
+                 ])
+    return  count_parth(point[0], point[1])
