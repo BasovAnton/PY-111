51b071d2186cc841d5517ef53ca63835f6150940
Администратор <anton.iv90@mail.ru>
2022-03-03 21:19:20 +0300

    03.03.2022

--- Tasks/e0_breadth_first_search.py
+++ Tasks/e0_breadth_first_search.py
@@ -1,14 +1,45 @@
 from typing import Hashable, List
+from collections import deque
+
 import networkx as nx
+import matplotlib.pyplot as plt
+
+
+def draw_graph(graph):
+    pos = nx.spring_layout(graph)
+    nx.draw_networkx_nodes(graph, pos)
+    nx.draw_networkx_labels(graph, pos)
+
+    for edge in graph.edges:
+        nx.draw_networkx_edges(
+            graph, pos,
+            edgelist=[(edge[0], edge[1])], connectionstyle="arc3,rad=0.2"
+            )
+
+    plt.show()
 
 
 def bfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
     """
     Do an breadth-first search and returns list of nodes in the visited order
-
     :param g: input graph
     :param start_node: starting node for search
     :return: list of nodes in the visited order
     """
-    print(g, start_node)
-    return list(g.nodes)
+    draw_graph(g)
+    path_nodes = []  # порядок обхода узлов
+    visited_nodes = {node: False for node in g.nodes}
+
+    wait_nodes = deque()  # очередь из узлов / горящие узлы
+    wait_nodes.append(start_node)  # поджигаю стартовый узел
+    visited_nodes[start_node] = True  # узел посетили
+    while wait_nodes:  # пока есть горящие узлы ...
+        current_node = wait_nodes.popleft()
+        neighbours = g[current_node]  # все соседи текущего узла
+        for neighbour in neighbours:
+            if not visited_nodes[neighbour]:
+                wait_nodes.append(neighbour)
+                visited_nodes[neighbour] = True
+        path_nodes.append(current_node)  # узел полностью сгорел
+
+    return path_nodes
--- Tasks/f0_binary_search_tree.py
+++ Tasks/f0_binary_search_tree.py
@@ -6,10 +6,37 @@ or with dicts (smth like {'key': 0, value: 123, 'left': {...}, 'right':{...}})
 from typing import Any, Optional, Tuple
 # import networkx as nx
 
+root = {
+    "key":8,
+    "value": 8,
+    "left": {
+        "key": 3,
+        "value": 3,
+        "left": None,
+        "right": None
+    },
+    "right": {
+        "key": 10,
+        "value": 10,
+        "left": None,
+        "right": None
+    },
+}
+
 
 class BinarySearchTree:
+    @staticmethod
+    def _create_node(key, value, left = None, right = None):
+
+        return {
+            "key": key,
+            "value": value,
+            "left": left,
+            "right": right
+        }
+
     def __init__(self):
-        ...
+        self.root = None
 
     def insert(self, key: int, value: Any) -> None:
         """
@@ -19,7 +46,27 @@ class BinarySearchTree:
         :param value: value associated with key
         :return: None
         """
-        print(key, value)
+        if self.root is None:
+            self.root = self._create_node(key, value)
+        else:
+            current_node = self.root
+            while True:
+                current_key = current_node["key"]
+                if key > current_key:
+                    # правое
+                    right_node = current_node["right"]
+                    if right_node is None:
+                        current_node["right"] = self._create_node(key, value)
+                        break
+                    current_node = current_node["right"]
+                else:
+                    left_node = current_node["left"]
+                    if left_node is None:
+                        current_node["left"] = self._create_node(key, value)
+                        break
+                    else:
+                        current_node = current_node["left"]
+
         return None
 
     def remove(self, key: int) -> Optional[Tuple[int, Any]]:
@@ -35,12 +82,21 @@ class BinarySearchTree:
     def find(self, key: int) -> Optional[Any]:
         """
         Find value by given key in the BST
-
         :param key: key for search in the BST
         :return: value associated with the corresponding key
         """
-        print(key)
-        return None
+
+        def _find(current_node: Optional[dict]):
+            if current_node is None:  # базовый случай
+                raise KeyError
+            current_key = current_node["key"]
+            if current_key == key:
+                return current_node["value"]
+
+            next_node = current_node["left"] if key < current_key else current_node["right"]
+            return _find(next_node)
+
+        return _find(self.root)
 
     def clear(self) -> None:
         """
